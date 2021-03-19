0bf2994d42b5977bef5e4d01ac1e7653b494f3ee
Emily Palinkas <Emilypalinkas@ou.edu>
2021-03-18 23:02:37 -0500

    redo new thing

---
+++ midterm redo/question 1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+
+/// <summary>
+/// Midterm Exam for MIS 3013 2020
+/// </summary>
+/// 
+/// <author>
+/// Emily Palinkas
+/// </author>
+
+namespace question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word >>");
+            string answer1 = Console.ReadLine();
+
+            Console.WriteLine("Enter a word >>");
+            string answer2 = Console.ReadLine();
+
+            int lettersword1 = 0, lettersword2 = 0;
+
+            for (int i = 0; i < answer1.Length; i++)
+            {
+                lettersword1++;
+
+            }
+
+            for (int i = 0; i < answer2.Length; i++)
+            {
+                lettersword2++;
+            }
+
+
+            Console.WriteLine($"There are {lettersword1.ToString("0.0")} letters in the word {answer1}");
+
+            Console.WriteLine($"There are {lettersword2.ToString("0.0")} letters in the word {answer2}");
+
+            Console.WriteLine("We will subtract the number of letters in the smallest word from the number of letters in the largest word");
+
+
+            int differenceinletters = 0;
+
+            if (lettersword1 >= lettersword2)
+            {
+                differenceinletters = lettersword1 - lettersword2;
+                Console.WriteLine($"{lettersword1.ToString("N1")} - {lettersword2.ToString("N1")} = {differenceinletters.ToString("N1")}");
+            }
+            else
+            {
+                differenceinletters = lettersword2 - lettersword1;
+                Console.WriteLine($"{lettersword2.ToString("N1")} - {lettersword1.ToString("N1")} = {differenceinletters.ToString("N1")}");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ midterm redo/question 2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+/// <summary>
+/// Midterm Exam for MIS 3013 2020
+/// </summary>
+/// 
+/// <author>
+/// Emily Palinkas
+/// </author>
+namespace question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many coins do you have total?");
+            string answer = Console.ReadLine();
+            int numberofcoins;
+
+
+            while (int.TryParse(answer, out numberofcoins) == false)
+            {
+                Console.WriteLine("please tell me the number of coins not fruit");
+                answer = Console.ReadLine();
+            }
+
+            double Total = 0;
+
+            for (int i = 0; i < numberofcoins; i++)
+            {
+                Console.WriteLine("What type of coin do you have?");
+                string typecoin = Console.ReadLine().ToLower();
+
+                if (typecoin == "penny")
+                {
+                    Total += 0.01;
+                }
+
+                if (typecoin == "nickel")
+                {
+                    Total += 0.05;
+                }
+
+                if (typecoin == "dime")
+                {
+                    Total += 0.10;
+                }
+
+                if (typecoin == "quarter")
+                {
+                    Total += 0.25;
+                }
+
+            }
+
+            Console.WriteLine($"you have {Total.ToString("C4")} Total!");
+        }
+    }
+}
